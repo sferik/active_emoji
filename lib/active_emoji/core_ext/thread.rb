ebe54a80a7a75495629207468cb0d958961a0ca3
Juan Paulo Gutierrez <juanpaulo.gutierrez@gmail.com>
2014-12-20 09:16:41 +0900

    :heavy_plus_sign::bug:

--- lib/active_emoji/core_ext/thread.rb
+++ lib/active_emoji/core_ext/thread.rb
@@ -1,1 +1,16 @@
-🐛 = Thread
+class Thread
+  alias 💓❓ alive?
+  alias 👥 group
+  alias ✅ inspect
+  alias 🈴 join
+  alias 🔑❓ key?
+  alias 🔑🔑 keys
+  alias 🔫 kill
+  alias 🙋 raise
+  alias 🏃 run
+  alias 👷 safe_level
+  alias 📝 status
+  alias ⛔❓ stop?
+  alias 💹 value
+  alias ⏰ wakeup
+end
