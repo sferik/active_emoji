02a5585e17e57649c7713bb7d43597289501aefd
John Mertens <john.mertens@gmail.com>
2018-10-22 22:12:19 -0700

    ➕ 🍿📞

--- lib/active_emoji/core_ext/kernel.rb
+++ lib/active_emoji/core_ext/kernel.rb
@@ -2,14 +2,18 @@ module Kernel
   alias 🆚 <=>
   alias 📎 binding
   alias 🔲❓ block_given?
+  alias 📞 caller
+  alias 🎣 catch
   alias 📥 gets
   alias 🔁 loop
+  alias 🅿️  p
   alias 📠 print
   alias 📤 puts
   alias 🎰 rand
   alias 👻 singleton_class
   alias 💤 sleep
   alias 💻 system
+  alias 🤮 throw
   alias ⚠️ warn
   def 🔟; 10 end
   def 💯; 100 end
